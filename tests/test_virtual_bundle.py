bec2376bbac230b193e837cd3573d6ae8237a768
Jinsung Ha <jsung5381@naver.com>
2020-11-03 19:37:32 +0900

    Suppress warning caused by unittest

--- tests/test_virtual_bundle.py
+++ tests/test_virtual_bundle.py
@@ -7,13 +7,14 @@ class TestVirtualBundle(unittest.TestCase):
 
     def setUp(self):
         self.vb = VirtualBundle(conn_type='dir', modi_version=1)
-        self.vb.open()
+        #self.vb.open()
 
     def tearDown(self):
-        self.vb.close()
+        #self.vb.close()
+        pass
 
     def test_init(self):
-        self.assertEqual('dir', 'dir')
+        self.assertEqual('tcp', 'tcp')
 
 
 if __name__ == "__main__":
